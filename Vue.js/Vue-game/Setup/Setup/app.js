08c9e9d0ddc0ade523a9a0527134af9ed5173d54
mitoo <mitoolab@gmail.com>
2018-01-26 23:46:22 +0900

    Vue-study_game_attack, specialAttack, heal, giveup click event

--- Vue.js/Vue-game/Setup/Setup/app.js
+++ Vue.js/Vue-game/Setup/Setup/app.js
@@ -4,5 +4,73 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false
+    turns: []
+  },
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    specialAttack: function() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }      
+      this.monsterAttacks();
+    },
+    heal: function() {
+      if (this.playerHealth <= 90) {
+          this.playerHealth += 10;
+      } else {
+          this.playerHealth = 100;
+      }
+      this.monsterAttacks();
+    },
+    giveUp: function() {
+      this.gameIsRunning = false;
+    },
+    monsterAttacks: function() {
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You win! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;         
+        }
+        return;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost...! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;         
+        }
+        return true;
+      }
+      return false;
+    }
   }
 });
